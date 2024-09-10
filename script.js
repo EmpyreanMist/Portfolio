f227497e0189598fec0835cb1d0656cfa15789a4
Christian Fryksten <christianfryksten1@gmail.com>
2024-09-10 13:51:55 +0200

    Added the basics of a quiz game

---
+++ script.js
@@ -0,0 +1,19 @@
+
+
+function darkMode() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+}
+
+
+/* Code for the pyramid generator */
+
+
+/* Variables */
+
+
+/* Code for the quiz game */
+
+function startGame() {
+  
+}
