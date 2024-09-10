c3f72738801ba2dc7d624e66dbdaf2d1b7a84deb
Christian Fryksten <christianfryksten1@gmail.com>
2024-09-10 19:02:43 +0200

    Fixing some stuff on the quiz game

--- script.js
+++ script.js
@@ -13,8 +13,8 @@ function startGame() {
  const restartGame = document.getElementById("restartGame");
 
  /* Changing the text to first question */
- const startText = document.getElementById("currentText");
- startText.innerText = "What is the fastest land animal?"
+ const currentText = document.getElementById("currentText");
+ currentText.innerText = "Which animal is known as the king of the jungle?"
 
 
 
@@ -31,27 +31,36 @@ function startGame() {
  restartGame.hidden = true;
 
  /* Changing the text on the 4 buttons to answers */
- answer1.innerText = "Cheetah";
- answer2.innerText = "Lion";
+ answer1.innerText = "Lion";
+ answer2.innerText = "Tiger";
  answer3.innerText = "Airplane";
- answer4.innerText = "Tiger";
+ answer4.innerText = "Elephant";
 
 }
 
-function firstAnswerCorrect() {
+function answerCorrect() {
 
  /* Declaring variables for the buttons */
   const answer1 = document.getElementById("answer1");
   const answer2 = document.getElementById("answer2");
   const answer3 = document.getElementById("answer3");
   const answer4 = document.getElementById("answer4");
+  const answer5 = document.getElementById("answer5");
+  const answer6 = document.getElementById("answer6");
+  const answer7 = document.getElementById("answer7");
+  const answer8 = document.getElementById("answer8");
+
   const nextQuestion = document.getElementById("nextQuestion");
 
-  /* Hiding all buttons but 1 */
+  /* Hiding all buttons but the next question one */
   answer1.hidden = true;
   answer2.hidden = true;
   answer3.hidden = true;
   answer4.hidden = true;
+  answer5.hidden = true;
+  answer6.hidden = true;
+  answer7.hidden = true;
+  answer8.hidden = true;
   nextQuestion.hidden = false;
 
 
@@ -59,21 +68,35 @@ function firstAnswerCorrect() {
   currentText.innerText = "Correct!";
 }
 
-function firstAnswerWrong() {
+function answerWrong() {
+
+  /* Declaring the buttons to variables */
 
   const answer1 = document.getElementById("answer1");
   const answer2 = document.getElementById("answer2");
   const answer3 = document.getElementById("answer3");
   const answer4 = document.getElementById("answer4");
+  const answer5 = document.getElementById("answer5");
+  const answer6 = document.getElementById("answer6");
+  const answer7 = document.getElementById("answer7");
+  const answer8 = document.getElementById("answer8");
+
   const restartGame = document.getElementById("restartGame");
   const currentText = document.getElementById("currentText");
 
+  /* Changing the text to show that the person has failed */
   currentText.innerText = "God damn you suck!";
+
+  /* Hiding all buttons except restartGame button */
 
   answer1.hidden = true;
   answer2.hidden = true;
   answer3.hidden = true;
   answer4.hidden = true;
+  answer5.hidden = true;
+  answer6.hidden = true;
+  answer7.hidden = true;
+  answer8.hidden = true;
   restartGame.hidden = false;
   
 
@@ -81,15 +104,53 @@ function firstAnswerWrong() {
 
 function nextQuestion() {
 
-  const answer1 = document.getElementById("answer1");
-  const answer2 = document.getElementById("answer2");
-  const answer3 = document.getElementById("answer3");
-  const answer4 = document.getElementById("answer4");
+  const answer5 = document.getElementById("answer5");
+  const answer6 = document.getElementById("answer6");
+  const answer7 = document.getElementById("answer7");
+  const answer8 = document.getElementById("answer8");
   const nextQuestion = document.getElementById("nextQuestion");
+  const currentText = document.getElementById("currentText");
   
   nextQuestion.hidden = true;
-  answer1.hidden = false;
-  answer2.hidden = false;
-  answer3.hidden = false;
-  answer4.hidden = false;
+  answer5.hidden = false;
+  answer6.hidden = false;
+  answer7.hidden = false;
+  answer8.hidden = false;
+
+  currentText.innerText = "What is the fastest land animal?";
+
+  answer5.innerText = "Lion";
+  answer6.innerText = "Gazelle";
+  answer7.innerText = "Cheetah";
+  answer8.innerText = "Horse";
+
+
+}
+
+function questionThree() {
+
+  const answer9 = document.getElementById("answer9");
+  const answer10 = document.getElementById("answer10");
+  const answer11 = document.getElementById("answer11");
+  const answer12 = document.getElementById("answer12");
+  const currentText = document.getElementById("currentText");
+
+
+
+  currentText.innerText = "Well done!";
+
+  answer5.hidden = true;
+  answer6.hidden = true;
+  answer7.hidden = true;
+  answer8.hidden = true;
+  
+
+  
+
+
+}
+
+
+function questionFour() {
+  
 }
