074d3eb571aa363df664b541eca1d4b117383f83
Christian Fryksten <christianfryksten1@gmail.com>
2024-09-24 13:57:21 +0200

    Fixed a darkMode button with localStorage

--- script.js
+++ script.js
@@ -1,63 +1,20 @@
+const body = document.querySelector("body");
+const darkModeToggleBtn = document.querySelector("#dark-mode-tgl-btn");
 
-
-
-/* Code for the quiz game */
-
-
-/* const bilar = ["BMW", "Volvo", "Mercedes", "Jaguar", "KIA", "Ferrari", "Volkswagen", "Rover"];
-
-
-
-for(let i = 0; i < bilar.length; i++) {
-
-  if(bilar[i] === "Rover") {
-    console.log(bilar[i]);
-  }
+if (localStorage.getItem('darkMode') === 'enabled') {
+  body.classList.add('dark-mode');
 }
 
- */
 
-/* let sum = 0;
-const personObject = {
-  firstName: "John",
-  lastName: "Smith",
-  age: 25 
-}; */
+darkModeToggleBtn.addEventListener("click", toggleDarkMode);
 
-/* numbers.forEach(myFunction);
 
-function myFunction(item) {
-  sum += item;
- 
-}
-console.log(sum);
- */
-/* const person = {
-  firstName: "Christian",
-  lastName: "Fryksten",
-  swede: true,
-  age: 26,
-  fullName: function(){
-    return this.firstName + " " + this.lastName;
+function toggleDarkMode() {
+  if (body.classList.contains('dark-mode')) {
+    body.classList.remove('dark-mode');
+    localStorage.setItem('darkMode', 'disabled');
+  } else {
+    body.classList.add('dark-mode');
+    localStorage.setItem('darkMode', 'enabled');
   }
-}; */
-
-/* Changing the name of the string */
-/* person.firstName = "Bert"; */
-
-/* Adding a variable into the object */
-/* person.homeTown = "Piteå";
-
-person.fruitBasket = [[1,2,3], {town: "Malmö"}, "Kiwis"]; */
-
-/*  console.log(person); 
-console.log(person.fruitBasket[1].town);
- */
-
-/* Prints the function inside person to give full name */
-/* console.log(person.fullName()); */
-
-
-
-
-//alert("Hello");
+}
